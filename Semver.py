9a52eaf42df547391ef632464d7aa120489dc9be
renjikari <fujiwara2097@gmail.com>
2018-02-28 11:14:41 +0900

    講義中やったところまでがここ

---
+++ Semver.py
@@ -0,0 +1,30 @@
+class Semver(object):
+
+
+    def __init__(self, major=0, minor=0, patch=0):
+        if major < 0 or minor < 0 or patch < 0:
+            raise TypeError
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __eq__(self, other):
+        return self.major == other.major and self.minor == other.minor and self.patch == other.patch
+
+    def __le__(self, other):
+        if self.major == other.major:
+            if self.minor == other.minor:
+                if self.patch > other.patch:
+                    return False
+            return self.minor <= other.minor
+        return self.major <= other.major
+
+    def __lt__(self, other):
+        if self.major == other.major:
+            if self.minor == other.minor:
+                if self.patch >= other.patch:
+                    return False
+                return True
+            return self.minor < other.minor
+        return self.major < other.major
+
---
+++ SemverTest.py
@@ -0,0 +1,77 @@
+import unittest
+from Semver import Semver
+
+
+class SemverMajorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.semver = Semver(1, 4, 2)
+
+    def test_プラスで初期化できること(self):
+        self.assertEqual(1, self.semver.major)
+        self.assertEqual(4, self.semver.minor)
+        self.assertEqual(2, self.semver.patch)
+
+    def test_マイナスで初期化できないこと(self):
+       with self.assertRaises(TypeError):
+           Semver(-1)
+       with self.assertRaises(TypeError):
+           Semver(1, -4,)
+       with self.assertRaises(TypeError):
+           Semver(1, 4, -2)
+
+
+    def test_整数型以外で初期化できないこと(self):
+       with self.assertRaises(TypeError):
+           Semver('1')
+       with self.assertRaises(TypeError):
+           Semver(1, '4')
+       with self.assertRaises(TypeError):
+           Semver(1, 4, '2')
+
+    def test_等価比較ができること(self):
+       self.assertTrue(self.semver == Semver(1, 4, 2))
+       self.assertFalse(self.semver == Semver(2, 4, 2))
+       self.assertFalse(self.semver == Semver(1, 8, 2))
+       self.assertFalse(self.semver == Semver(1, 4, 4))
+
+    def test_不等価比較ができること(self):
+       self.assertTrue(self.semver != Semver(2, 4, 2))
+       self.assertTrue(self.semver != Semver(1, 8, 2))
+       self.assertTrue(self.semver != Semver(1, 4, 4))
+
+    def test_以上の比較ができること(self):
+       self.assertTrue(self.semver >= Semver(1, 4, 2))
+       self.assertTrue(self.semver >= Semver(0, 4, 2))
+       self.assertTrue(self.semver >= Semver(1, 2, 2))
+       self.assertTrue(self.semver >= Semver(1, 4, 0))
+
+       self.assertFalse(self.semver >= Semver(2, 4, 2))
+       self.assertFalse(self.semver >= Semver(1, 8, 2))
+       self.assertFalse(self.semver >= Semver(1, 4, 4))
+
+    def test_以下の比較ができること(self):
+       self.assertTrue(self.semver <= Semver(1, 4, 2))
+       self.assertTrue(self.semver <= Semver(2, 4, 2))
+       self.assertTrue(self.semver <= Semver(1, 8, 2))
+       self.assertTrue(self.semver <= Semver(1, 4, 4))
+
+       self.assertFalse(self.semver <= Semver(0, 4, 2))
+       self.assertFalse(self.semver <= Semver(1, 2, 2))
+       self.assertFalse(self.semver <= Semver(1, 4, 0))
+
+    def test_より大きいの比較ができること(self):
+       self.assertFalse(self.semver > Semver(1, 4, 2))
+       self.assertTrue(self.semver > Semver(0, 4, 2))
+       self.assertTrue(self.semver > Semver(1, 2, 2))
+       self.assertTrue(self.semver > Semver(1, 4, 0))
+
+    def test_より小さいの比較ができること(self):
+       self.assertFalse(self.semver < Semver(1, 4, 2))
+       self.assertTrue(self.semver < Semver(2, 4, 2))
+       self.assertTrue(self.semver < Semver(1, 8, 2))
+       self.assertTrue(self.semver < Semver(1, 4, 4))
+
+
+if __name__ == "__main__":
+    unittest.main()
