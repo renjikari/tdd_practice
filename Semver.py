7529d5bdcb5ddf4c84e34692243389fd8203478d
renjikari <fujiwara2097@gmail.com>
2018-02-28 13:25:32 +0900

    等価比較をリストで実行

--- Semver.py
+++ Semver.py
@@ -17,7 +17,7 @@ class Semver(object):
         self.patch = patch
 
     def __eq__(self, other):
-        return self.major == other.major and self.minor == other.minor and self.patch == other.patch
+        return self.semver == [other.major, other.minor, other.patch]
 
     def __le__(self, other):
         if self.major == other.major:
