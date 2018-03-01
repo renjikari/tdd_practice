799b7c1e098b6e9b7f46da947f16c81e66916088
renjikari <fujiwara2097@gmail.com>
2018-03-01 17:39:19 +0900

    testクラスを分割した

--- SemverTest.py
+++ SemverTest.py
@@ -4,29 +4,49 @@ from Semver import Semver
 
 class SemverMajorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.semver = Semver(1, 4, 2)
-
     def test_プラスで初期化できること(self):
-        self.assertEqual(1, self.semver.major)
-        self.assertEqual(4, self.semver.minor)
-        self.assertEqual(2, self.semver.patch)
+        self.assertEqual(1, Semver(major=1).major)
 
     def test_整数型以外で初期化するとTypeErrorが返ってくること(self):
        with self.assertRaises(TypeError):
-           Semver('1')
-       with self.assertRaises(TypeError):
-           Semver(1, '4')
-       with self.assertRaises(TypeError):
-           Semver(1, 4, '2')
+           Semver(major = '1')
 
     def test_マイナスで初期化するとValueErrorが返ってくること(self):
        with self.assertRaises(ValueError):
-           Semver(-1)
+           Semver(major = -1)
+
+class SemverMinorTest(unittest.TestCase):
+
+    def test_プラスで初期化できること(self):
+        self.assertEqual(1, Semver(minor=1).minor)
+
+    def test_整数型以外で初期化するとTypeErrorが返ってくること(self):
+       with self.assertRaises(TypeError):
+           Semver(minor = '1')
+
+    def test_マイナスで初期化するとValueErrorが返ってくること(self):
        with self.assertRaises(ValueError):
-           Semver(1, -4,)
+           Semver(minor = -1)
+
+
+class SemverPatchTest(unittest.TestCase):
+
+    def test_プラスで初期化できること(self):
+        self.assertEqual(1, Semver(patch=1).patch)
+
+    def test_整数型以外で初期化するとTypeErrorが返ってくること(self):
+       with self.assertRaises(TypeError):
+           Semver(patch = '1')
+
+    def test_マイナスで初期化するとValueErrorが返ってくること(self):
        with self.assertRaises(ValueError):
-           Semver(1, 4, -2)
+           Semver(patch = -1)
+
+
+class SemverEqualTest(unittest.TestCase):
+
+    def setUp(self):
+        self.semver = Semver(1, 4, 2)
 
     def test_等価比較ができること(self):
        self.assertTrue(self.semver == Semver(1, 4, 2))
@@ -38,6 +58,11 @@ class SemverMajorTest(unittest.TestCase):
        self.assertTrue(self.semver != Semver(2, 4, 2))
        self.assertTrue(self.semver != Semver(1, 8, 2))
        self.assertTrue(self.semver != Semver(1, 4, 4))
+
+class SemverLeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.semver = Semver(1, 4, 2)
 
     def test_以上の比較ができること(self):
        self.assertTrue(self.semver >= Semver(1, 4, 2))
@@ -58,6 +83,12 @@ class SemverMajorTest(unittest.TestCase):
        self.assertFalse(self.semver <= Semver(0, 4, 2))
        self.assertFalse(self.semver <= Semver(1, 2, 2))
        self.assertFalse(self.semver <= Semver(1, 4, 0))
+
+
+class SemverLtTest(unittest.TestCase):
+
+    def setUp(self):
+        self.semver = Semver(1, 4, 2)
 
     def test_より大きいの比較ができること(self):
        self.assertFalse(self.semver > Semver(1, 4, 2))
